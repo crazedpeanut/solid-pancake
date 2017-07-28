d7725b068ac42e4b003f076be90d9202aed7982b
John Kendall <john.kendall@team.telstra.com>
2017-07-28 22:40:42 +1000

    Init

---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    context: __dirname + "/src",
+    entry: "./index",
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    }
+};
