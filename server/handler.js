03b80eb8ed13c7771c001e473f0b79b9b4501dd7
Shaun Walker <shaungwalker@gmail.com>
2017-07-29 16:18:21 +1000

    Add serverless implementation of the server stuff

---
+++ server/handler.js
@@ -0,0 +1,71 @@
+'use strict';
+
+console.log('Loading function');
+
+const doc = require('dynamodb-doc');
+
+const dynamo = new doc.DynamoDB();
+
+
+module.exports.pubs = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin' : '*'
+        },
+    });
+
+    switch (event.httpMethod) {
+        case 'GET':
+            dynamo.scan({
+                TableName : 'pubs',
+                Limit : 550
+            }, done);
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+};
+
+module.exports.pubcrawls = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin' : '*'
+        },
+    });
+
+    switch (event.httpMethod) {
+        case 'DELETE':
+            dynamo.deleteItem(JSON.parse(event.body), done);
+            break;
+        case 'GET':
+
+
+            dynamo.scan({ TableName: "pubcrawls" }, done);
+            break;
+        case 'POST':
+            var post_data = JSON.parse(event.body);
+
+            var params = {
+                TableName: 'pubcrawls',
+                Item:  post_data
+            }
+
+            dynamo.putItem(params, done);
+            break;
+        case 'PUT':
+            dynamo.updateItem(JSON.parse(event.body), done);
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+};
