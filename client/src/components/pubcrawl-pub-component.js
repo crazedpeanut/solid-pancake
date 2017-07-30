7de5b379992e96f44c20cf943b3c1a11c603b0ec
Shaun Walker <shaungwalker@gmail.com>
2017-07-30 14:42:23 +1000

    broken stuff

--- client/src/components/pubcrawl-pub-component.js
+++ client/src/components/pubcrawl-pub-component.js
@@ -1,9 +1,71 @@
 const React = require('react');
 
+let map, service;
+
+function initMap(pub) {
+    const central_park = new google.maps.LatLng(40.764243, -73.973049);
+    console.log('pub: '+pub.PubID);
+    console.log(central_park);
+    console.log(getPubDivID(pub.PubID));
+    console.log(document.getElementById(getPubDivID(pub.PubID)));
+
+    map = new google.maps.Map(document.getElementById(getPubDivID(pub.PubID)), {
+        center: central_park,
+        zoom: 15
+    });
+
+    const request = {
+        location: central_park,
+        radius: "500",
+        types: ["food"]
+    };
+
+    //service = new google.maps.places.PlacesService(map);
+    //service.nearbySearch(request, searchResult);
+}
+
+function searchResult(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        // show first result on map and request for details
+        const place = results[0];
+        const marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name
+        });
+        const infowindow = new google.maps.InfoWindow({
+            content: place.name
+        });
+        infowindow.open(map, marker);
+
+        service.getDetails({placeId: place.place_id}, function(place, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                let rating = document.querySelector('#rating');
+                let reviewEl = document.querySelector('.review-list');
+
+                rating.innerHTML = place.rating;
+
+                for (let review of place.reviews){
+                    let li = document.createElement('li');
+                    li.innerHTML = `<div>Author: ${review.author_name}</div>
+                          <em>${review.text}</em>
+                          <div>Rating: ${review.rating} star(s)</div>`;
+                    reviewEl.appendChild(li);
+                }
+            }
+        });
+    }
+}
+
+
 function getPubImg(name) {
     const val = (name.charCodeAt(0) % 3) + 1;
 
     return `/assets/img/PubIcon${val}.png`;
+}
+
+function getPubDivID(id) {
+    return `pub${id}`;
 }
 
 class PubCrawlPubComponent extends React.Component {
@@ -17,7 +79,7 @@ class PubCrawlPubComponent extends React.Component {
     render() {
         return(
 
-            <div>
+            <div id={getPubDivID(this.props.pub.PubID)}>
                 <br/><br/>
                 <div className="ui grid" id="pub1">
                     <div className="six wide column">
@@ -43,6 +105,7 @@ class PubCrawlPubComponent extends React.Component {
                 <div id="map"></div>
                 <hr/>
                 <div><strong>Overall rating:</strong> <span id="rating"></span></div>
+                {initMap(this.props.pub)}
             </div>
         
         );
